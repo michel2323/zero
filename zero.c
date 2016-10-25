be9383f87ea7ae80102fa981ee3bd2de7facc934
Michel Schanen <mschanen@anl.gov>
2016-10-25 17:33:28 -0500

    mpi in there

--- zero.c
+++ zero.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include <mpi.h>
 #include "zero_compression.h"
 
 double * generate_nzeros(double *res,int n, int pzeros) {
@@ -48,9 +49,11 @@ void diff(double *data1, double *data2, int size) {
 }
 
 int main(int argc, char * argv[]) {
+  MPI_Init(&argc,&argv);
   int ierr;
   int pzeros=0;
   int n=0;
+  int rank=0;
   if(argc==1) {
     n=134217728;
   }
@@ -63,26 +66,35 @@ int main(int argc, char * argv[]) {
   double *ddata=malloc(sizeof(double)*n);
   char *cdata=malloc(sizeof(double)*n);
 
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
   for(pzeros=10;pzeros<=100;pzeros=pzeros+10) {
+    MPI_Barrier(MPI_COMM_WORLD);
     double t0=omp_get_wtime(); 
     generate_nzeros(data,n,pzeros);
     double t1=omp_get_wtime(); 
-    printf("%f seconds for generating data\n",t1-t0);
+    if(rank==0)
+      printf("%f seconds for generating data\n",t1-t0);
+    MPI_Barrier(MPI_COMM_WORLD);
     t0=omp_get_wtime(); 
     zero_compress_(data, &n, cdata, &m, &ierr);
     t1=omp_get_wtime(); 
-    printf("%f seconds for compressing data\n",t1-t0);
+    if(rank==0)
+      printf("%f seconds for compressing data\n",t1-t0);
     if(m>n*8) {
       printf("Compressed data is bigger than uncompressed data! %d %d\n", n*8,m);
     }
     long int nl=(long int) n;
+    MPI_Barrier(MPI_COMM_WORLD);
     t0=omp_get_wtime(); 
     zero_decompress(cdata, ddata, &nl, &ierr);
     t1=omp_get_wtime(); 
-    printf("%f seconds for decompressing data\n",t1-t0);
+    if(rank==0)
+      printf("%f seconds for decompressing data\n",t1-t0);
     diff(data,ddata,n);
   }
   free(data); free(ddata), free(cdata);
+  MPI_Finalize();
 
 }
 
